69d9e6999165d6a058cc48bbe25c2c4c70a29c36
gishi523 <gishi523@gmail.com>
2018-11-11 02:32:14 +0900

    Refactor stereo_kitti

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -18,142 +18,118 @@
 * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <numeric>
 
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<iomanip>
-#include<chrono>
-#include<thread>
+#include <opencv2/opencv.hpp>
 
-#include<opencv2/opencv.hpp>
-
-#include<System.h>
-
-using namespace std;
+#include <System.h>
 
 static inline void usleep(int64_t usec) { std::this_thread::sleep_for(std::chrono::microseconds(usec)); }
 
-void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
-                vector<string> &vstrImageRight, vector<double> &vTimestamps);
+template <class... Args>
+static std::string FormatString(const char* fmt, Args... args)
+{
+	const int BUF_SIZE = 1024;
+	char buf[BUF_SIZE];
+	std::snprintf(buf, BUF_SIZE, fmt, args...);
+	return std::string(buf);
+}
+
+static int LoadImages(const char* path, std::vector<std::string>& I0s, std::vector<std::string>& I1s,
+	std::vector<double>& timestamps)
+{
+	std::ifstream ifs(FormatString("%s/times.txt", path));
+	CV_Assert(!ifs.fail());
+
+	double timestamp;
+	while (ifs >> timestamp)
+		timestamps.push_back(timestamp);
+
+	const int nframes = static_cast<int>(timestamps.size());
+	I0s.resize(nframes);
+	I1s.resize(nframes);
+	for (int i = 0; i < nframes; i++)
+	{
+		I0s[i] = FormatString("%s/image_0/%06d.png", path, i);
+		I1s[i] = FormatString("%s/image_1/%06d.png", path, i);
+	}
+	return nframes;
+}
 
 int main(int argc, char **argv)
 {
-    if(argc != 4)
-    {
-        cerr << endl << "Usage: ./stereo_kitti path_to_vocabulary path_to_settings path_to_sequence" << endl;
-        return 1;
-    }
+	if (argc < 4)
+	{
+		std::cerr << "Usage: ./stereo_kitti path_to_vocabulary path_to_settings path_to_sequence [use_viewer]" << std::endl;
+		return 1;
+	}
 
-    // Retrieve paths to images
-    vector<string> vstrImageLeft;
-    vector<string> vstrImageRight;
-    vector<double> vTimestamps;
-    LoadImages(string(argv[3]), vstrImageLeft, vstrImageRight, vTimestamps);
+	// Retrieve paths to images
+	std::vector<std::string> ILs;
+	std::vector<std::string> IRs;
+	std::vector<double> timestamps;
 
-    const int nImages = vstrImageLeft.size();
+	const int nframes = LoadImages(argv[3], ILs, IRs, timestamps);
 
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    auto SLAM = ORB_SLAM2::System::Create(argv[1],argv[2],ORB_SLAM2::System::STEREO,true);
+	// Create SLAM system. It initializes all system threads and gets ready to process frames.
+	const bool useViewer = argc > 4 ? std::stoi(argv[4]) != 0 : true;
+	auto SLAM = ORB_SLAM2::System::Create(argv[1], argv[2], ORB_SLAM2::System::STEREO, useViewer);
 
-    // Vector for tracking time statistics
-    vector<float> vTimesTrack;
-    vTimesTrack.resize(nImages);
+	// Vector for tracking time statistics
+	std::vector<double> trackTimes;
+	trackTimes.resize(nframes);
 
-    cout << endl << "-------" << endl;
-    cout << "Start processing sequence ..." << endl;
-    cout << "Images in the sequence: " << nImages << endl << endl;   
+	std::cout << std::endl << "-------" << std::endl;
+	std::cout << "Start processing sequence ..." << std::endl;
+	std::cout << "Images in the sequence: " << nframes << std::endl << std::endl;
 
-    // Main loop
-    cv::Mat imLeft, imRight;
-    for(int ni=0; ni<nImages; ni++)
-    {
-        // Read left and right images from file
-        imLeft = cv::imread(vstrImageLeft[ni],CV_LOAD_IMAGE_UNCHANGED);
-        imRight = cv::imread(vstrImageRight[ni],CV_LOAD_IMAGE_UNCHANGED);
-        double tframe = vTimestamps[ni];
+	// Main loop
+	for (int i = 0; i < nframes; i++)
+	{
+		// Read left and right images from file
+		const cv::Mat IL = cv::imread(ILs[i], cv::IMREAD_UNCHANGED);
+		const cv::Mat IR = cv::imread(IRs[i], cv::IMREAD_UNCHANGED);
+		const double timestamp = timestamps[i];
 
-        if(imLeft.empty())
-        {
-            cerr << endl << "Failed to load image at: "
-                 << string(vstrImageLeft[ni]) << endl;
-            return 1;
-        }
+		if (IL.empty())
+		{
+			std::cerr << std::endl << "Failed to load image at: " << ILs[i] << std::endl;
+			return 1;
+		}
 
-		std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+		const auto t1 = std::chrono::steady_clock::now();
 
-        // Pass the images to the SLAM system
-        SLAM->TrackStereo(imLeft,imRight,tframe);
+		// Pass the images to the SLAM system
+		SLAM->TrackStereo(IL, IR, timestamp);
 
-		std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+		const auto t2 = std::chrono::steady_clock::now();
 
-        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+		const double T1 = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
+		const double T2 = i < nframes - 1 ? timestamps[i + 1] - timestamp : timestamp - timestamps[i - 1];
 
-        vTimesTrack[ni]=ttrack;
+		trackTimes[i] = T1;
 
-        // Wait to load the next frame
-        double T=0;
-        if(ni<nImages-1)
-            T = vTimestamps[ni+1]-tframe;
-        else if(ni>0)
-            T = tframe-vTimestamps[ni-1];
+		// Wait to load the next frame
+		if (T1 < T2)
+			usleep(static_cast<int64_t>(1e6 * (T2 - T1)));
+	}
 
-        if(ttrack<T)
-            usleep((T-ttrack)*1e6);
-    }
+	// Stop all threads
+	SLAM->Shutdown();
 
-    // Stop all threads
-    SLAM->Shutdown();
+	// Tracking time statistics
+	std::sort(std::begin(trackTimes), std::end(trackTimes));
+	const double totalTime = std::accumulate(std::begin(trackTimes), std::end(trackTimes), 0.);
 
-    // Tracking time statistics
-    sort(vTimesTrack.begin(),vTimesTrack.end());
-    float totaltime = 0;
-    for(int ni=0; ni<nImages; ni++)
-    {
-        totaltime+=vTimesTrack[ni];
-    }
-    cout << "-------" << endl << endl;
-    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
-    cout << "mean tracking time: " << totaltime/nImages << endl;
+	std::cout << "-------" << std::endl << std::endl;
+	std::cout << "median tracking time: " << trackTimes[nframes / 2] << std::endl;
+	std::cout << "mean tracking time: " << totalTime / nframes << std::endl;
 
-    // Save camera trajectory
-    SLAM->SaveTrajectoryKITTI("CameraTrajectory.txt");
+	// Save camera trajectory
+	SLAM->SaveTrajectoryKITTI("CameraTrajectory.txt");
 
-    return 0;
-}
-
-void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
-                vector<string> &vstrImageRight, vector<double> &vTimestamps)
-{
-    ifstream fTimes;
-    string strPathTimeFile = strPathToSequence + "/times.txt";
-    fTimes.open(strPathTimeFile.c_str());
-    while(!fTimes.eof())
-    {
-        string s;
-        getline(fTimes,s);
-        if(!s.empty())
-        {
-            stringstream ss;
-            ss << s;
-            double t;
-            ss >> t;
-            vTimestamps.push_back(t);
-        }
-    }
-
-    string strPrefixLeft = strPathToSequence + "/image_0/";
-    string strPrefixRight = strPathToSequence + "/image_1/";
-
-    const int nTimes = vTimestamps.size();
-    vstrImageLeft.resize(nTimes);
-    vstrImageRight.resize(nTimes);
-
-    for(int i=0; i<nTimes; i++)
-    {
-        stringstream ss;
-        ss << setfill('0') << setw(6) << i;
-        vstrImageLeft[i] = strPrefixLeft + ss.str() + ".png";
-        vstrImageRight[i] = strPrefixRight + ss.str() + ".png";
-    }
+	return 0;
 }
