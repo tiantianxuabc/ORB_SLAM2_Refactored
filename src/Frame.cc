29849c081b6f4d5c17bed57a286c3712c7085cdc
gishi523 <gishi523@gmail.com>
2018-11-17 22:08:44 +0900

    Clean code

--- src/Frame.cc
+++ src/Frame.cc
@@ -180,10 +180,6 @@ Frame::Frame(const Frame &frame)
 	mvScaleFactors(frame.mvScaleFactors), mvInvScaleFactors(frame.mvInvScaleFactors),
 	mvLevelSigma2(frame.mvLevelSigma2), mvInvLevelSigma2(frame.mvInvLevelSigma2), grid(frame.grid)
 {
-	/*for (int i = 0; i < FRAME_GRID_COLS; i++)
-		for (int j = 0; j < FRAME_GRID_ROWS; j++)
-			mGrid[i][j] = frame.mGrid[i][j];*/
-
 	if (!frame.mTcw.empty())
 		SetPose(frame.mTcw);
 }
