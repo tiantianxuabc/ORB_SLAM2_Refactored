33093e8056d2884708a2c2cd09308b7a6c64a14e
gishi523 <gishi523@gmail.com>
2018-11-25 10:15:38 +0900

    Clean code

--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -20,25 +20,24 @@
 
 #include "Optimizer.h"
 
+#include <mutex>
+
+#include <Thirdparty/g2o/g2o/core/block_solver.h>
+#include <Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.h>
+#include <Thirdparty/g2o/g2o/solvers/linear_solver_eigen.h>
+#include <Thirdparty/g2o/g2o/types/types_six_dof_expmap.h>
+#include <Thirdparty/g2o/g2o/core/robust_kernel_impl.h>
+#include <Thirdparty/g2o/g2o/solvers/linear_solver_dense.h>
+#include <Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h>
+
+#include <Eigen/StdVector>
+
 #include "Map.h"
 #include "MapPoint.h"
 #include "KeyFrame.h"
 #include "LoopClosing.h"
 #include "Frame.h"
-
-#include "Thirdparty/g2o/g2o/core/block_solver.h"
-#include "Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.h"
-#include "Thirdparty/g2o/g2o/solvers/linear_solver_eigen.h"
-#include "Thirdparty/g2o/g2o/types/types_six_dof_expmap.h"
-#include "Thirdparty/g2o/g2o/core/robust_kernel_impl.h"
-#include "Thirdparty/g2o/g2o/solvers/linear_solver_dense.h"
-#include "Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h"
-
-#include<Eigen/StdVector>
-
 #include "Converter.h"
-
-#include<mutex>
 
 namespace ORB_SLAM2
 {
